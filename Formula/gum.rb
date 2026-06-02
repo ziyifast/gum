1290837d4f6f10489780813f69ce5f828db0dc19
ziyi <ziyi@163.com>
2026-06-02 20:08:24 +0800

    chore: update SHA256 for v1.1.0

--- Formula/gum.rb
+++ Formula/gum.rb
@@ -2,7 +2,7 @@ class Gum < Formula
   desc "Switch between multiple Git identities (user.name, user.email, SSH keys) with one command"
   homepage "https://github.com/ziyifast/gum"
   url "https://github.com/ziyifast/gum/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  sha256 "5d6d251018669125ac144723a8e80d1236cab90bd570c3ae7a58d6b9eee1ddfb"
   license "MIT"
   version "1.1.0"
 
